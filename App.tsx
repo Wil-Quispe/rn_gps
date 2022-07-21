be244320d158b96d8563554c6f3dc8b870d24f04
Wil-Quispe <newwilcode@gmail.com>
2022-07-20 19:18:35 -0500

    install apollo client

--- App.tsx
+++ App.tsx
@@ -1,41 +1,17 @@
-import {View, Text, PermissionsAndroid} from 'react-native';
-import React, {useEffect} from 'react';
-import Geolocation from '@react-native-community/geolocation';
+import React from 'react';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
+import Home from './src/screens/Signup';
 
-const App = () => {
-  useEffect(() => {
-    const requestLocation = async () => {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: 'location Access Required',
-            message: 'this app needs to access your location',
-            buttonPositive: 'Accept',
-          },
-        );
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'localhost:5000',
+  cache: new InMemoryCache(),
+});
 
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          Geolocation.getCurrentPosition(
-            position => {
-              console.log({position});
-            },
-            error => console.log('error denied'),
-            {enableHighAccuracy: true, timeout: 25000, maximumAge: 3600000},
-          );
-        }
-      } catch (err) {
-        console.log({err});
-      }
-    };
-    requestLocation();
-  }, []);
-
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
-};
+const App = () => (
+  <ApolloProvider client={client}>
+    <Home />
+  </ApolloProvider>
+);
 
 export default App;
